0ef25002539e0ab57a1d61c98a3cb58b77f57af0
Krishna Kumar <kks32@gmx.com>
2015-07-23 16:38:41 +0100

    initial commit

---
+++ slide_config.js
@@ -0,0 +1,38 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+      title: 'Underwater collapse of granular columns',
+      subtitle: 'Discrete Element - Lattice Boltzmann Coupling',
+      //eventInfo: {
+      //  title: 'Google I/O',
+      //  date: '6/x/2013'
+      //},
+      useBuilds: true, // Default: true. False will turn off slide animation builds.
+      usePrettify: true, // Default: true
+      enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+      enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+      //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+      favIcon: 'images/theme/kings_icon.png',
+      fonts: [
+          'Open Sans:regular,semibold,italic,italicsemibold',
+          'Source Code Pro'
+      ],
+      //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+
+      //Workshop
+      workshop: '13 USNCCM 2015, <br/>San Diego, CA',
+  },
+
+    // Author information
+    presenters: [{
+        name: 'Krishna Kumar',
+        company: '',
+        email: 'kks32@cam.ac.uk'
+    }, {
+        name: 'Prof. Kenichi Soga',
+        company: 'University of Cambridge, UK',
+    },{
+        name: 'Prof. Jean-Yves Delenne',
+        company: 'IATE, UMR1208 INRA-CIRAD-Montpellier Supagro-UM2, France',
+    }]
+};
